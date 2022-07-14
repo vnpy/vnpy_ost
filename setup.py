609c785c9b909128d721cd655791ae0629824a01
noranhe <63988546+noranhe@users.noreply.github.com>
2022-07-14 15:45:31 +0800

    [Mod] setup.py说明

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ from setuptools import Extension, setup
 def get_ext_modules() -> list:
     """
     获取三方模块
-
+    Linux和Windows需要编译封装接口
     Mac由于缺乏二进制库支持无法使用
     """
-
     if platform.system() == "Linux":
         extra_compile_flags = [
             "-std=c++17",
